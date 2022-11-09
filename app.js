33b0ffb456963d1ea64e2d5d62f8543fb7174972
AJ Murray <aj.murr4y@gmail.com>
2022-11-09 11:40:10 -0500

    linked ejs and rendered a test page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const path = require('path')
 const app = express()
 dotenv.config()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req,res)=> {
+    res.render('home')
+})
 
 app.listen(process.env.PORT, () => [
     console.log(`Listening on port: ${process.env.PORT}`)
