8c1df23fea76ad7d485d0f3155817a1b5c7346c4
AJ Murray <aj.murr4y@gmail.com>
2022-11-09 11:57:52 -0500

    connected app to local database and created a model

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const path = require('path')
+const mongoose = require('mongoose')
+
 const app = express()
 dotenv.config()
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Connection error'))
+db.once('open', () => {
+    console.log('Database connected')
+})
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req,res)=> {
+app.get('/', (req, res) => {
     res.render('home')
 })
 
